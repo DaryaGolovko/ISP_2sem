cb927059d4c887c0ace4219fa7434dcf35954eef
DaryaGolovko <73973382+DaryaGolovko@users.noreply.github.com>
2021-04-23 06:17:46 +0300

    Add files via upload

---
+++ lab6/Enum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab6
+{
+    enum Actions
+    {
+        Exercizes = 1,
+        Recomendations,
+        MassCounter,
+        ShowRecords
+    }
+}
